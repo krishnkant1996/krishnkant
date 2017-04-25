5c0a3875ed384c0e10afa82d78cb5d1fbd6f0ebb
Krishnkant Tiwari <Krishnkant Tiwari>
2017-04-25 23:12:55 +0530

    new

---
+++ prog_size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+        {
+
+                printf("\n size of integer = %d \n ", sizeof(int));
+                printf("\n size of long intrger = %d \n ", sizeof(long int));
+                printf("\n size of float = %d \n ", sizeof(float));
+                printf("\n size of double = %d \n ", sizeof(double));
+                printf("\n size of chareter = %d \n ", sizeof(char));
+                printf("\n size of integer pointer = %d \n ", sizeof(int *));
+                printf("\n size of chareter pointer = %d \n ", sizeof(char *));
+                printf("\n size of float pointer = %d \n ", sizeof(float *));
+                printf("\n size of pointer to pointer = %d \n ", sizeof(int **));
+                printf("\n size of short integer= %d \n ", sizeof(short int));
+                return 0;
+        }
